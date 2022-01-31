fce4be59b958996be0c153bfabbccd7d9f56fb50
arpannnnn <94790089+arpannnnn@users.noreply.github.com>
2022-01-31 13:09:03 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -12,4 +12,5 @@ $(document).ready(function(){
 //toggle menu/navbar script
 $('.menu-btn').click(function(){
     $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i ').toggleClass("active");
 });
