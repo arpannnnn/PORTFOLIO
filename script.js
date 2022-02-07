fc609604fa6fe71b6fef16ce207a1d3a938c3482
arpannnnn <94790089+arpannnnn@users.noreply.github.com>
2022-02-07 11:48:30 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -7,8 +7,41 @@ $(document).ready(function(){
             else{
                 $(".navbar").removeClass("sticky");
             }
+            if (this.scrollY > 500){
+                $('.scroll-up-btn').addClass("show");
+            }
+            else{
+                $('.scroll-up-btn').removeClass("show");
+            }
         })
     });
+    //slide up 
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0})
+    });
+    //toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    //typing animation
+    var typed=new Typed(".typing",{
+         strings: ["Youtuber","Developer","Coder","Designer","Freelancer"],
+         typedSpeed:30,
+         backSpeed:60,
+         loop:true,
+
+    });
+    var typed=new Typed(".typing-2",{
+        strings: ["Youtuber","Developer","Coder","Designer","Freelancer"],
+        typedSpeed:30,
+        backS peed:60,
+        loop:true,
+
+   });
+
+
 //toggle menu/navbar script
 $('.menu-btn').click(function(){
     $('.navbar .menu').toggleClass("active");
