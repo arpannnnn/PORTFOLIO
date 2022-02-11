1b6924c82dc8c4bb28b39735554907a4758fd71f
ArpanAcharya <94790089+arpannnnn@users.noreply.github.com>
2022-02-11 19:07:58 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,40 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY >20){
+            if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
         }
-
-            else{
-                $(".navbar").removeClass("sticky");
-            }
-            if (this.scrollY > 500){
-                $('.scroll-up-btn').addClass("show");
-            }
-            else{
-                $('.scroll-up-btn').removeClass("show");
-            }
-        })
+          if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
-    //slide up 
+    // slide-up 
     $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop:0})
+        $('html').animate({scrollTop: 0});
+     $('html').css("scrollBehavior", "auto");
     });
-    //toggle menu/navbar script
+    $('.navbar .menu li a').click(function(){
+     $('html').css("scrollBehavior", "smooth");
+    });
+    // toggle menu//navbar 
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-
-    //typing animation
-    var typed=new Typed(".typing",{
-         strings: ["Youtuber","Developer","Coder","Designer","Freelancer"],
-         typedSpeed:30,
-         backSpeed:60,
-         loop:true,
-
+    // typing animation 
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Developer", "Coder", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
-    var typed=new Typed(".typing-2",{
-        strings: ["Youtuber","Developer","Coder","Designer","Freelancer"],
-        typedSpeed:30,
-        backSpeed:60,
-        loop:true,
-
-   });
-
-
-//toggle menu/navbar script
-$('.menu-btn').click(function(){
-    $('.navbar .menu').toggleClass("active");
-    $('.menu-btn i ').toggleClass("active");
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Developer", "coder", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 });
