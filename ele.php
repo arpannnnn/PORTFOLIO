59fb757bf2e53202cbc54038e83245ac3b54257e
ArpanAcharya <94790089+arpannnnn@users.noreply.github.com>
2022-02-26 17:28:25 +0545

    Add files via upload

--- ele.php
+++ ele.php
@@ -1,19 +1,58 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "arpannacharya@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-   
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+<?php
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    $filenameee =  $_FILES['file']['name'];
+    $fileName = $_FILES['file']['tmp_name']; 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $usermessage = $_POST['message'];
+    
+    $message ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $usermessage; 
+    
+    $subject ="My email subject";
+    $fromname ="My Website Name";
+    $fromemail = 'noreply@codeconia.com';  //if u dont have an email create one on your cpanel
+
+    $mailto = 'arpannacharya.com';  //the email which u want to recv this email
+
+
+
+
+    $content = file_get_contents($fileName);
+    $content = chunk_split(base64_encode($content));
+
+    // a random hash will be necessary to send mixed content
+    $separator = md5(time());
+
+    // carriage return type (RFC)
+    $eol = "\r\n";
+
+    // main header (multipart mandatory)
+    $headers = "From: ".$fromname." <".$fromemail.">" . $eol;
+    $headers .= "MIME-Version: 1.0" . $eol;
+    $headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
+    $headers .= "Content-Transfer-Encoding: 7bit" . $eol;
+    $headers .= "This is a MIME encoded message." . $eol;
+
+    // message
+    $body = "--" . $separator . $eol;
+    $body .= "Content-Type: text/plain; charset=\"iso-8859-1\"" . $eol;
+    $body .= "Content-Transfer-Encoding: 8bit" . $eol;
+    $body .= $message . $eol;
+
+    // attachment
+    $body .= "--" . $separator . $eol;
+    $body .= "Content-Type: application/octet-stream; name=\"" . $filenameee . "\"" . $eol;
+    $body .= "Content-Transfer-Encoding: base64" . $eol;
+    $body .= "Content-Disposition: attachment" . $eol;
+    $body .= $content . $eol;
+    $body .= "--" . $separator . "--";
+
+    //SEND Mail
+    if (mail($mailto, $subject, $body, $headers)) {
+        echo "mail send ... OK"; // do what you want after sending the email
+        
+        
+    } else {
+        echo "mail send ... ERROR!";
+        print_r( error_get_last() );
     }
-?>
